74189401ad9114d453411c8aa3bad93ae6bee364
Dmytrii Furs <furs.dmitry@gmail.com>
2022-04-15 17:42:49 -0400

    templaye, working on it

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
-  title = "JD Planner";
+  title = "College Planner";
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
