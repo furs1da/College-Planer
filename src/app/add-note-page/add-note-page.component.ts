ed9d12230664bde79b353f38b0861595567df284
Dmytrii Furs <furs.dmitry@gmail.com>
2022-04-16 17:17:29 -0400

    add note done

--- src/app/add-note-page/add-note-page.component.ts
+++ src/app/add-note-page/add-note-page.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {Note} from "../models/notes.model";
+import {Assignment} from "../models/assignments.model";
+import {Course} from "../models/courses.model";
+import {DatabaseService} from "../services/database.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-note-page',
@@ -9,12 +13,45 @@ import {Note} from "../models/notes.model";
 export class AddNotePageComponent implements OnInit {
   formTitle = 'Add Note';
   note:Note = new Note();
-  constructor() { }
+  courses: Course[] = [];
+  courseId: number = -1;
+  assignments: Assignment[] = [];
+  selectAssignmentDisabled:boolean = true;
+
+  constructor(private database: DatabaseService,
+              private router: Router, private ngZone: NgZone) { }
 
   ngOnInit(): void {
+    this.database.selectAllCourses().then((data)=>{
+      this.courses = data;
+    }).catch((error)=>{
+      console.error(error)
+    });
+  }
+
+  onCourseChange(event){
+    this.courseId = event;
+    this.selectAssignmentDisabled = false;
+    this.note.assignmentId = undefined;
+
+    this.database.selectAllAssignmentsByCourse(this.courseId).then((data)=>{
+      this.assignments = data;
+    }).catch((error)=>{
+      console.error(error)
+    });
+  }
+
+  onAssignmentChange(event){
+    this.note.assignmentId = event;
   }
 
   btnSave_click(){
-
+    this.database.insertNote(this.note, () => {
+      console.log("Record added successfully");
+      alert("Record added successfully");
+      this.ngZone.run(() => {
+        this.router.navigate(['listNote/']);
+      });
+    });
   }
 }
