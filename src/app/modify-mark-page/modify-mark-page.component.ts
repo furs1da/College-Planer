e3567076762762d2173f6f004b4c58d6eefca2a6
j1yang <87741372+j1yang@users.noreply.github.com>
2022-04-16 12:35:58 -0400

    Update modify-mark-page.component.ts

--- src/app/modify-mark-page/modify-mark-page.component.ts
+++ src/app/modify-mark-page/modify-mark-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {DatabaseService} from "../services/database.service";
+import {Mark} from "../models/marks.model";
+import {Assignment} from "../models/assignments.model";
+import {Course} from "../models/courses.model";
 
 @Component({
   selector: 'app-modify-mark-page',
@@ -6,10 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modify-mark-page.component.css']
 })
 export class ModifyMarkPageComponent implements OnInit {
+  mark: Mark = new Mark();
 
-  constructor() { }
+  courses: Course[] = []
+  assignments: Assignment[] = []
+
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private database: DatabaseService,
+              private router: Router,
+              private ngZone: NgZone) { }
 
   ngOnInit(): void {
+    let id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    console.log(`id is ${id}`);
+    this.database.selectMark(id)
+      .then((data) => {
+        console.info(data);
+        this.mark = data;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+
+
   }
 
+  formTitle = 'Update Mark';
+
+
+  btnUpdate_click() {
+    this.database.updateMark(this.mark, () => {
+      console.log("Mark updated successfully");
+      alert("Mark updated successfully");
+    });
+    this.ngZone.run(() => {
+      this.router.navigate(['listMark']);
+    });
+  }
+
+  btnCancel_click(){
+    this.ngZone.run(() => {
+      this.router.navigate(['listMark']);
+    });
+  }
 }
