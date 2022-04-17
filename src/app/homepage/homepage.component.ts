1c182cdafc3f9bcc523f7646d1518b796933e263
Dmytrii Furs <furs.dmitry@gmail.com>
2022-04-17 03:08:43 -0400

    api almsot done

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {Quote} from "../models/quote.model";
 
 
 @Component({
@@ -6,13 +7,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
+
+
+
+
+
 export class HomepageComponent implements OnInit {
   title = 'Project Planner';
-
-
+  quote:Quote = new Quote();
+  randomNumber:number = Math.random();
   constructor() { }
 
   ngOnInit(): void {
+    this.quote = new Quote();
+    this.randomNumber = Math.random();
+
+    fetch("https://type.fit/api/quotes")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+
+
+      });
   }
 
 }
---
+++ src/app/models/quote.model.ts
@@ -0,0 +1,10 @@
+export class Quote {
+  text: string = "";
+  author: string = "";
+
+  constructor(text?: string,
+              author?: string) {
+    this.text = text;
+    this.author = author;
+  }
+}
