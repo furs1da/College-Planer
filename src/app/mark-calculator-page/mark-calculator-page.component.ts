b18a00fc06526f955bb73759aede67cf4034c26c
j1yang <87741372+j1yang@users.noreply.github.com>
2022-04-16 23:49:48 -0400

    Create mark-calculator-page.component.ts

---
+++ src/app/mark-calculator-page/mark-calculator-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mark-calculator-page',
+  templateUrl: './mark-calculator-page.component.html',
+  styleUrls: ['./mark-calculator-page.component.css']
+})
+export class MarkCalculatorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
